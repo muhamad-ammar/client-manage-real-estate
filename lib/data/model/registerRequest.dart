855e44fbc8db458c4f08fc6664d51ef522cab15c
muhamad-ammar <ammar.bscs18seecs@seecs.edu.pk>
2024-03-20 23:32:41 +0500

    Update registerRequest.dart

--- lib/data/model/registerRequest.dart
+++ lib/data/model/registerRequest.dart
@@ -1,37 +1,53 @@
 class RegisterRequest {
   String? login;
   String? firstName;
+  String? fullName;
   String? lastName;
   String? email;
+  String? mobile1;
+  String? mobile2;
   String? password;
-  String? gender;
+  String? description;
+  String? role;
 
   RegisterRequest({
     this.login,
     this.firstName,
+    this.fullName,
     this.lastName,
     this.email,
+    this.mobile1,
+    this.mobile2,
     this.password,
-    this.gender,
+    this.description,
+    this.role,
   });
 
   RegisterRequest.fromJson(Map<String, dynamic> json) {
     login = json['login'];
     firstName = json['first_name'];
+    fullName = json['full_name'];
     lastName = json['last_name'];
     email = json['email'];
+    mobile1 = json['mobile1'];
+    mobile2 = json['mobile2'];
     password = json['password'];
-    gender = json['user_gender'];
+    description = json['description'];
+    role = json['role'];
   }
 
   Map<String, dynamic> toJson() {
     final data = <String, dynamic>{};
     data['login'] = login;
     data['first_name'] = firstName;
+    data['full_name'] = fullName;
     data['last_name'] = lastName;
     data['email'] = email;
+    data['mobile1'] = mobile1;
+    data['mobile2'] = mobile2;
     data['password'] = password;
-    data['gender'] = gender ?? "";
+    data['description'] = description;
+    data['role'] = role ?? "";
     return data;
   }
 }
