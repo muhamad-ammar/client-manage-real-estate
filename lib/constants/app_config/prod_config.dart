b01802f0ed2b6b3c0798050d2d0b3350dee7b30c
muhamad-ammar <ammar.bscs18seecs@seecs.edu.pk>
2024-03-20 14:08:24 +0500

    Update prod_config.dart

--- lib/constants/app_config/prod_config.dart
+++ lib/constants/app_config/prod_config.dart
@@ -2,7 +2,7 @@ import 'base_config.dart';
 
 class ProdConfig implements BaseConfig {
   @override
-  String get apiHost => "https://crash-course.cyclic.app/";
+  String get apiHost => "https://property-help.vercel.app/";
 
   @override
   String get baseURL => apiHost;
