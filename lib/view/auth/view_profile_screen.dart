677185c54d04b0db28a889bfcb6b7c994d5bc660
muhamad-ammar <ammar.bscs18seecs@seecs.edu.pk>
2024-03-20 14:07:49 +0500

    Create view_profile_screen.dart

---
+++ lib/view/auth/view_profile_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:property_help/Utils/app_styles.dart';
+import 'package:property_help/Utils/uiUtils.dart';
+import 'package:property_help/data/model/User.dart';
+
+import '../../Utils/appTheme.dart';
+
+class ViewProfileScreen extends StatefulWidget {
+  @override
+  _ViewProfileScreenState createState() => _ViewProfileScreenState();
+}
+
+class _ViewProfileScreenState extends State<ViewProfileScreen> {
+  // Replace with your initial user data
+  User? userData;
+
+  // Simulate fetching data (replace with actual data fetching logic)
+
+  @override
+  void initState() {
+    super.initState();
+    userData = User(
+        name: "Muhammad Ammar",
+        id: "tm0001",
+        mobile1: "03226258404",
+        email: "ammar@gmail.com",
+        role: "Manager",
+        assignees: ["cl000001", "cl000005"]); // Fetch data on initialization
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container( width: UiUtils.getScreenWidth(context)*0.9,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage(""),
+              ),
+              Insets.gapH20,
+
+              Text(
+                userData?.name ?? "",
+                style: Textify.heading1,
+              ),
+              Insets.gapH5,
+              // Email
+              Text(userData?.role ?? "", style: Textify.heading2.copyWith(color: AppTheme.darkGrayColor)),
+              Insets.gapH5,
+              Text(userData?.mobile1 ?? "", style: Textify.heading2.copyWith(color: AppTheme.darkGrayColor)),
+              Insets.gapH5
+              // Username
+
+              // Add more profile details here (e.g., bio, location)
+              // ...
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
