ebb2141e7655757d6d8ffb27f68dd220df7e8f18
muhamad-ammar <ammar.bscs18seecs@seecs.edu.pk>
2024-03-20 14:08:13 +0500

    Update routesNames.dart

--- lib/constants/routesNames.dart
+++ lib/constants/routesNames.dart
@@ -4,6 +4,8 @@ class RoutesName {
   static const String signIn = 'signin';
   static const String courseContent = 'courseContent';
   static const String testContent = 'testContent';
+  static const String register = 'register';
   static const String contactUs = 'contactUs';
+  static const String mainScreen = 'mainScreen';
 
 }
